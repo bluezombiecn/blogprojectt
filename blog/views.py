47504eefdfc48933ea70f9f42b5eedee50768a9f
blue <bluezombiecn@163.com>
2020-02-21 08:44:22 +0000

    init

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from .models import Post
+
+
+def index(request):
+    post_list = Post.objects.all().order_by('-create_time')
+    return render(request,'blog/index.html',context={'post_list':post_list})
