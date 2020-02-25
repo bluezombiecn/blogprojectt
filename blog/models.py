5b90ef4f1a8e1611f201c3eb4a7270ee09319f7f
blue <bluezombiecn@163.com>
2020-02-25 17:55:23 +0800

    update

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+    
+
  
  
 class Category(models.Model):  
@@ -10,6 +14,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
  
  
 class Tag(models.Model):
@@ -18,16 +26,31 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
-class Post(models.Model):
-    title = models.CharField(max_length=70)
-    body = models.TextField()
-    created_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
-    excerpt = models.CharField(max_length=200, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name  
 
+class Post(models.Model):
+    title = models.CharField('标题', max_length=70)
+    body = models.TextField('正文')
+    created_time = models.DateTimeField('创建时间',default=timezone.now)
+    modified_time = models.DateTimeField('修改时间')
+    excerpt = models.CharField('摘要', max_length=200, blank=True)
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
+    author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE) 
+
+    def save(self,*args, **kwargs):
+        self.modified_time = timezone.now()
+        super().save(*args,**kwargs)
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:detail", kwargs={"pk": self.pk})
+    
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
 
@@ -7,5 +7,10 @@ from .models import Post
 
 
 def index(request):
-    post_list = Post.objects.all().order_by('-create_time')
-    return render(request,'blog/index.html',context={'post_list':post_list})
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request,"blog/index.html",context={"post_list": post_list })
+
+
+def detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/detail.html',context={'post':pos})
