5f5a12b814070746535cc468c63e693da4617770
blue <bluezombiecn@163.com>
2020-02-27 09:22:49 +0000

    update

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
-from .models import Post
+from .models import Post,Category,Tag
 import markdown
 import re
 from django.utils.text import slugify
@@ -27,3 +27,18 @@ def detail(request,pk):
 
     return render(request,'blog/detail.html',context={'post':post})
 
+def archive(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def tag(request, pk):
+    # 记得在开始部分导入 Tag 类
+    t = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=t).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
---
+++ comments/templatetags/comments_extras.py
@@ -0,0 +1,14 @@
+from django import template
+from ..forms import CommentForm
+ 
+register = template.Library()
+ 
+ 
+@register.inclusion_tag('comments/inclusions/_form.html', takes_context=True)
+def show_comment_form(context, post, form=None):
+    if form is None:
+        form = CommentForm()
+    return {
+        'form': form,
+        'post': post,
+    }
